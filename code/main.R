a016aebfab30ebe97d46dba00cced47fd3ba53f6
Dan Reuman <reuman@ku.edu>
2022-04-04 16:25:13 -0500

    fleshed out main.R into work packages

--- code/main.R
+++ code/main.R
@@ -2,5 +2,21 @@
 
 
 
+
 #Import and clean the data and save in results as data.RData
 source("CleanDat.R")
+
+
+#Work package 1 (Jasmin/Adeola): Plotting Vcom, Vsym, phiLdM, etc versus diversity, and related work
+
+
+
+#Work package 2 (Adeola/Vadim) Taylor's law
+
+
+
+#Work package 3: (Nat/Jasmin) Under/overyielding
+
+
+#Work package 4: (Dan/Nat, also Lei later) Parallel analysis to Lei's ms
+
