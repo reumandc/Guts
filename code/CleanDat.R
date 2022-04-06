a802c6cd3f3889f1a23b64ec8f0565ab34e93e08
jasminalbert <pimsupa_jasmin@ku.edu>
2022-04-06 13:43:58 -0500

    responding to Dans comments

--- code/CleanDat.R
+++ code/CleanDat.R
@@ -4,9 +4,10 @@
 #cleaned data is a list whose length is equal to the 
 #number of individuals
 #list elements are data.frames for each individual
-#columns are microbe genera, rows are samples
-#***DAN: jasmin, please add statement here about what the rows are, and what the
-#difference is between NAs and 0s. Then delete this comment.
+#columns are microbe genera, rows are days
+#NAs are gaps in the data (row-wide; days without samples) 
+#and 0s mean no cells detected 
+
 
 rm(list=ls())
 
@@ -19,12 +20,7 @@ if (dir.exists(res_loc)==FALSE){
 ##import tab deliminated .txt file
 dat <- read.delim("../data/QMPs.txt")
 
-#rows are samples and 1st column is sample ID followed by 
-#***DAN: I am not sure it is true that rows are samples. It looks like, for a
-#given subject, you'll have NAs under all genera if you have NAs under any of them.
-#That suggests rows are days, with NAs when there was no sample that day. The rows
-#cannot be samples, because you cannot have a sample with all NAs for all genera,
-#what would that mean? After fixing, pls delete this comment.
+#rows are days and 1st column is sample ID followed by 
 #columns of genera
 #individuals are stacked as rows 
 
@@ -34,17 +30,12 @@ colnames(dat)[1] <- 'sample'
 
 #individuals have different total samples
 #each individual is identified by a unique integer followed by 
-#a decimal and sample number 
-#example: 1.1 is individual one sample one 
-#***DAN: I do not think this is quite right because there is a 1.00, so I guess
-#that is the first sample, not 1.1.
+#a decimal and sample number, starting at 0 
+#example: 1.1 is individual one day two 
 #use this to separate individuals into list elements
 
 ##individual order in raw data is: 
-#ind_ID <- c(1:11, 13, 15:17, 12, 18:21) #no 14
-ind_ID<-unique(floor(dat[[1]]))
-#***DAN: Please check this change, which gives the same thing as the commented line.
-#If the change looks OK, please delete this comment and the commented line.
+ind_ID<-unique(floor(dat[[1]])) #no 14
 
 datList <- vector(mode='list', length=21)
 
@@ -53,14 +44,8 @@ for (i in 1:length(datList)){
 }
 
 datList <- datList[-14]
-#names(datList) <- c(1:11, 13, 15:17, 12, 18:21)
 names(datList)<-ind_ID
-#***DAN: Jasmin, pls check this change and if it looks OK delete this comment 
-#and the commented line
 
-#***DAN: Jasmin, please check the below addition, which just makes sure
-#that whenever there is an NA for one genus, you get an NA for all of them,
-#and delete this comment if this addition looks OK.
 #check that, for a given subject, whenever the microbial genus entries 
 #are either all of no NAs
 for (subject in 1:length(datList))
