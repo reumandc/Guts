c22c4185df53864671f7424ea1d3933f6dbd82e7
Dan Reuman <reuman@ku.edu>
2022-04-06 12:36:24 -0500

    some more minor changes and cmments to CleanDat.R, for Jasmin's review

--- code/CleanDat.R
+++ code/CleanDat.R
@@ -5,6 +5,8 @@
 #number of individuals
 #list elements are data.frames for each individual
 #columns are microbe genera, rows are samples
+#***DAN: jasmin, please add statement here about what the rows are, and what the
+#difference is between NAs and 0s. Then delete this comment.
 
 rm(list=ls())
 
@@ -18,6 +20,11 @@ if (dir.exists(res_loc)==FALSE){
 dat <- read.delim("../data/QMPs.txt")
 
 #rows are samples and 1st column is sample ID followed by 
+#***DAN: I am not sure it is true that rows are samples. It looks like, for a
+#given subject, you'll have NAs under all genera if you have NAs under any of them.
+#That suggests rows are days, with NAs when there was no sample that day. The rows
+#cannot be samples, because you cannot have a sample with all NAs for all genera,
+#what would that mean? After fixing, pls delete this comment.
 #columns of genera
 #individuals are stacked as rows 
 
@@ -29,10 +36,15 @@ colnames(dat)[1] <- 'sample'
 #each individual is identified by a unique integer followed by 
 #a decimal and sample number 
 #example: 1.1 is individual one sample one 
+#***DAN: I do not think this is quite right because there is a 1.00, so I guess
+#that is the first sample, not 1.1.
 #use this to separate individuals into list elements
 
 ##individual order in raw data is: 
-ind_ID <- c(1:11, 13, 15:17, 12, 18:21) #no 14
+#ind_ID <- c(1:11, 13, 15:17, 12, 18:21) #no 14
+ind_ID<-unique(floor(dat[[1]]))
+#***DAN: Please check this change, which gives the same thing as the commented line.
+#If the change looks OK, please delete this comment and the commented line.
 
 datList <- vector(mode='list', length=21)
 
@@ -41,8 +53,10 @@ for (i in 1:length(datList)){
 }
 
 datList <- datList[-14]
-names(datList) <- c(1:11, 13, 15:17, 12, 18:21)
-
+#names(datList) <- c(1:11, 13, 15:17, 12, 18:21)
+names(datList)<-ind_ID
+#***DAN: Jasmin, pls check this change and if it looks OK delete this comment 
+#and the commented line
 
 ##save
 saveRDS(datList, file=paste0(res_loc, "dataList.Rds"))
