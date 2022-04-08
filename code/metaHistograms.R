18722fa8261f59c950d6abe691605f6023f35269
jasmin <pimsupa_jasmin@ku.edu>
2022-04-08 13:00:38 -0500

    finish metadata histogram scripting

--- code/metaHistograms.R
+++ code/metaHistograms.R
@@ -1,3 +1,5 @@
+#getting to know the data: make metadata histograms
+
 #results location
 res_loc <- "../results/"
 
@@ -20,6 +22,8 @@ red <- rgb(0.75,0,0,0.3)
 
 breaks <- seq(min(samples),max(days), length.out=13)
 
+pdf(paste0(fig_loc, "histDaysSamples.pdf"), height=5, width=6)
+
 hist(days, breaks=breaks, col=blue, main="number of days and samples per subject", border=blue, 
      xlab="number of days or samples", cex.main=1.6)
 abline(v=mean(days), col=rgb(0,0,0.5), lty=2, lwd=1.5)
@@ -27,11 +31,23 @@ hist(samples, breaks=breaks, add=TRUE, col=red, border=red)
 abline(v=mean(samples), col=rgb(0.5,0,0), lty=2, lwd=1.5)
 legend("topright", legend=c("days","samples"), fill=c(blue, red), bty='n', border=c(blue,red), cex=1.3)
 
+dev.off()
 
+#number of microbes in a gut
+microbes <- sapply(dat, function(X){nrow(X[!colSums(X, na.rm=T)==0,])})
 
+green <- rgb(0, 0.75, 0, 0.5)
 
+breaks <- seq(min(microbes), max(microbes), length.out=11)
 
+pdf(paste0(fig_loc, "histMicrobes.pdf"), height=5, width=6)
 
+hist(microbes, breaks=breaks, col=green, border=green, 
+     main=paste("total microbe genera=", ncol(dat[[1]]),
+                "\nmean=", mean(microbes), "; sd=", round(sd(microbes),2)),
+     xlab="number of microbes in a gut", cex.main=1.4)
+rug(microbes)
 
+dev.off()
 
 
