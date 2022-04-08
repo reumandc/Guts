fc047896592647bcecc0477d18300586c35ddf39
jasmin <pimsupa_jasmin@ku.edu>
2022-04-08 12:06:10 -0500

    adding to hist code

--- code/variousHistograms.R
+++ code/variousHistograms.R
@@ -15,6 +15,19 @@ if (dir.exists(fig_loc)==F){
 days <- sapply(dat, nrow)
 samples <- sapply(dat, function(X){nrow(X[!is.na(rowSums(X)),])})
 
+blue <- rgb(0,0,0.75,0.3)
+red <- rgb(0.75,0,0,0.3)
+
+#breaks <- seq(min(samples),max(days))
+
+hist(days, col=blue, main="number of days and sample per subject", border=blue, 
+     xlab="number of days or samples", cex.main=1.6)
+abline(v=mean(days), col=rgb(0,0,0.5), lty=2, lwd=1.5)
+hist(samples, add=TRUE, col=red, border=red)
+abline(v=mean(samples), col=rgb(0.5,0,0), lty=2, lwd=1.5)
+legend("topright", legend=c("days","samples"), fill=c(blue, red), bty='n', 
+       border=c(blue,red), cex=1.3)
+
 
 
 
