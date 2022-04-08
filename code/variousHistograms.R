a1a8fbffda0eaad326587b8eb0e4abb5b1d47e8a
jasminalbert <pimsupa_jasmin@ku.edu>
2022-04-08 12:34:53 -0500

    adding code

--- code/variousHistograms.R
+++ code/variousHistograms.R
@@ -18,15 +18,14 @@ samples <- sapply(dat, function(X){nrow(X[!is.na(rowSums(X)),])})
 blue <- rgb(0,0,0.75,0.3)
 red <- rgb(0.75,0,0,0.3)
 
-#breaks <- seq(min(samples),max(days))
+breaks <- seq(min(samples),max(days), length.out=13)
 
-hist(days, col=blue, main="number of days and sample per subject", border=blue, 
+hist(days, breaks=breaks, col=blue, main="number of days and samples per subject", border=blue, 
      xlab="number of days or samples", cex.main=1.6)
 abline(v=mean(days), col=rgb(0,0,0.5), lty=2, lwd=1.5)
-hist(samples, add=TRUE, col=red, border=red)
+hist(samples, breaks=breaks, add=TRUE, col=red, border=red)
 abline(v=mean(samples), col=rgb(0.5,0,0), lty=2, lwd=1.5)
-legend("topright", legend=c("days","samples"), fill=c(blue, red), bty='n', 
-       border=c(blue,red), cex=1.3)
+legend("topright", legend=c("days","samples"), fill=c(blue, red), bty='n', border=c(blue,red), cex=1.3)
 
 
 
