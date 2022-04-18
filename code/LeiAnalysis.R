94b295d0f8c8eb9895ff9f3e8563510254963f3e
Dan Reuman <reuman@ku.edu>
2022-04-18 12:24:19 -0500

    added code to compute most of the metrics of Lei, still need richness simpsons, and evenness

--- code/LeiAnalysis.R
+++ code/LeiAnalysis.R
@@ -28,7 +28,8 @@ source("Spop.R")
 source("varrat.R")
 
 #***now calculate all the quantities of Lei for each subject
-leires<-data.frame(subject=1:length(dat),Scom=NA,Spop=NA,Scomip=NA)
+
+leires<-data.frame(subject=1:length(dat),Scom=NA,Spop=NA,Scomip=NA,SAE=NA,CPE=NA,varrat=NA,PhiVert=NA)
 for (subject in 1:length(dat))
 {
   sdat<-dat[[subject]]
@@ -36,10 +37,45 @@ for (subject in 1:length(dat))
   #remove days when no sample was available, relies on the check in CleanDat
   #that either all or none of the data are missing for a day
   sdat<-sdat[-which(is.na(sdat[,1])),]
+  if (any(is.na(sdat)))
+  {
+    stop("Error in LeiAnalysis: checkpoint 1")
+  }
   
   #remove genera which were never present
   sdat<-sdat[,-which(apply(FUN=max,X=sdat,MARGIN=2)==0)]
   
   #now fill in the values
-  #***DAN: still need to do this after writing functions
+  leires[subject,"Scom"]<-Scom(sdat)
+  leires[subject,"Spop"]<-Spop(sdat)
+  leires[subject,"Scomip"]<-Scomip(sdat)
+  leires[subject,"SAE"]<-SAE(sdat)
+  leires[subject,"CPE"]<-CPE(sdat)
+  leires[subject,"varrat"]<-varrat(sdat)
+  leires[subject,"PhiVert"]<-PhiVert(sdat)
 }
+
+#***check a few identities
+
+if (any(abs(leires$Scom-leires$PhiVert*leires$Spop)>1e-12))
+{
+  stop("Error in LeiAnalysis: checkpoint 2")
+}
+if (any(abs(leires$PhiVert-leires$SAE*leires$CPE)>1e-12))
+{
+  stop("Error in LeiAnalysis: checkpoint 3")
+}
+if (any(abs(leires$Scomip-leires$SAE*leires$Spop)>1e-12))
+{
+  stop("Error in LeiAnalysis: checkpoint 4")
+}
+if (any(abs(leires$CPE-1/sqrt(leires$varrat))>1e-12))
+{
+  stop("Error in LeiAnalysis: checkpoint 5")
+}
+
+#***now make plots to compare our results to those of Lei
+
+
+
+
