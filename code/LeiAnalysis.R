7723746ed7bfef005e06142c9b8a14dbf2c9b291
Dan Reuman <reuman@ku.edu>
2022-04-18 14:37:08 -0500

    added a simpson's index and some additions to the Lei analysis

--- code/LeiAnalysis.R
+++ code/LeiAnalysis.R
@@ -26,10 +26,13 @@ source("Scom.R")
 source("Scomip.R")
 source("Spop.R")
 source("varrat.R")
+source("shannon.R")
+source("simpson.R")
 
 #***now calculate all the quantities of Lei for each subject
 
-leires<-data.frame(subject=1:length(dat),Scom=NA,Spop=NA,Scomip=NA,SAE=NA,CPE=NA,varrat=NA,PhiVert=NA,richness1=NA,days=NA)
+leires<-data.frame(subject=1:length(dat),Scom=NA,Spop=NA,Scomip=NA,SAE=NA,CPE=NA,varrat=NA,PhiVert=NA,
+                   richness1=NA,richness2=NA,shannon1=NA,shannon2=NA,simpson1=NA,simpson2=NA,days=NA)
 for (subject in 1:length(dat))
 {
   sdat<-dat[[subject]]
@@ -55,6 +58,10 @@ for (subject in 1:length(dat))
   leires[subject,"PhiVert"]<-PhiVert(sdat)
   leires[subject,"richness1"]<-dim(sdat)[2] #total number of taxa ever seen
   leires[subject,"richness2"]<-mean(apply(FUN=function(x){sum(x>0)},MARGIN=1,X=sdat)) #average across time of numbers detected in each sample
+  leires[subject,"shannon1"]<-shannon(apply(FUN=mean,X=sdat,MARGIN=2)) #take the mean abundance of each species and then compute shannon
+  leires[subject,"shannon2"]<-mean(apply(FUN=shannon,MARGIN=1,X=sdat)) #compute shannon for each time step and then average across time
+  leires[subject,"simpson1"]<-simpson(apply(FUN=mean,X=sdat,MARGIN=2)) #take the mean abundance of each species and then compute simpson
+  leires[subject,"simpson2"]<-mean(apply(FUN=simpson,MARGIN=1,X=sdat)) #compute simpson for each time step and then average across time
   leires[subject,"days"]<-dim(sdat)[1]
 }
 
@@ -81,13 +88,47 @@ if (any(abs(leires$CPE-1/sqrt(leires$varrat))>1e-12))
 
 #***make a few comparisons between measures of diversity
 
-plot(leires$richness1,leires$richness2,type="p")
-RichnessCor<-cor(leires$richness1,leires$richness2) #
+pdf(file=paste0(resloc,"CorrelationBetweenTwoMeasuresOfRichness.pdf"))
+plot(leires$richness1,leires$richness2,type="p",xlab="richness 1",ylab="richness 2")
+dev.off()
+RichnessCor<-cor(leires$richness1,leires$richness2) 
 saveRDS(RichnessCor,paste0(resloc,"CorrelationBetweenTwoMeasuresOfRichness.Rds"))
 
+pdf(file=paste0(resloc,"CorrelationBetweenTwoMeasuresOfShannon.pdf"))
+plot(leires$shannon1,leires$shannon2,type="p",xlab="shannon 1",ylab="shannon 2")
+dev.off()
+ShannonCor<-cor(leires$shannon1,leires$shannon2) 
+saveRDS(ShannonCor,paste0(resloc,"CorrelationBetweenTwoMeasuresOfShannon.Rds"))
 
+pdf(file=paste0(resloc,"CorrelationBetweenTwoMeasuresOfSimpson.pdf"))
+plot(leires$simpson1,leires$simpson2,type="p",xlab="simpson 1",ylab="simpson 2")
+dev.off()
+SimpsonCor<-cor(leires$simpson1,leires$simpson2) 
+saveRDS(SimpsonCor,paste0(resloc,"CorrelationBetweenTwoMeasuresOfSimpson.Rds"))
+
+
+pdf(file=paste0(resloc,"RichnessVsShannon1.pdf"))
+plot(leires$richness1,leires$shannon1,type="p",xlab="richness 1",ylab="shannon 1")
+dev.off()
+RichnessShannon1Cor<-cor(leires$richness1,leires$shannon1)
+saveRDS(RichnessShannon1Cor,paste0(resloc,"CorRichnessShannon1.Rds"))
+
+pdf(file=paste0(resloc,"RichnessVsInvSimpson1.pdf"))
+plot(leires$richness1,1/leires$simpson1,type="p",xlab="richness 1",ylab="1/(simpson 1)")
+dev.off()
+RichnessInvSimpson1Cor<-cor(leires$richness1,1/leires$simpson1)
+saveRDS(RichnessInvSimpson1Cor,paste0(resloc,"CorRichnessInvSimpson1.Rds"))
+
+pdf(file=paste0(resloc,"ShannonVsInvSimpson1.pdf"))
+plot(leires$shannon1,1/leires$simpson1,type="p",xlab="shannon 1",ylab="1/(simpson 1)")
+dev.off()
+ShannonInvSimpson1Cor<-cor(leires$shannon1,1/leires$simpson1)
+saveRDS(ShannonInvSimpson1Cor,paste0(resloc,"CorShannonInvSimpson1.Rds"))
+
+#The upshot of the above is basically that it should not matter much what index you use
 
 #***now make plots to compare our results to those of Lei
+
 
 
 
--- code/simpson.R
+++ code/simpson.R
@@ -1,21 +1,21 @@
-#Shannon's diversity index
+#Simpson's index
 #
 #Args
 #abund          A vector of abundances of species, no NAs. If there are any NAs it throws the
-#                 error "Error in shannon: NAs present"
+#                 error "Error in simpson: NAs present"
 #
 #Output - the index, a single number
 #
-shannon<-function(abund)
+simpson<-function(abund)
 {
   #error checking
   if (any(is.na(abund)))
   {
-    stop("Error in shannon: NAs present")
+    stop("Error in simpson: NAs present")
   }
   
   #calculations
   p<-abund/sum(abund)
   p<-p[p!=0]
-  return(-sum(p*log(p)))
+  return(sum(p^2))
 }
